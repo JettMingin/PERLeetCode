0adb99790b3c4bc7a27e530e791aa3d7645972e2
JettMingin <155850120+JettMingin@users.noreply.github.com>
2024-01-27 12:01:14 -0500

    Update TwoSum.pl

--- TwoSum.pl
+++ TwoSum.pl
@@ -6,15 +6,9 @@ use strict;
 my @nums = (2, 7, 11, 15);
 my $target = 22;
 
-my $comchk = 0;
-
 sub sum_finder{
     my $parser = $_[1] + 1;  
-    foreach my $i ($parser..$#nums){  #"parser" key to less than On^2?  replace parser w 0 to see
-
-        #$comchk ++;                    #complexity check
-        #say "$comchk TEST";
-
+    foreach my $i ($parser..$#nums){  
         if ($_[0] == $nums[$i]){
             return $i;
         }
@@ -23,12 +17,9 @@ sub sum_finder{
 
 foreach my $i (0..$#nums){
     my $diff = $target - $nums[$i];
-
     my $j = &sum_finder($diff, $i);
     if ($j){
         say "$i, $j";
         last;
     }
 }
-
-#O(n^2) :( - (not quite? array being parsed is decreasing by 1 element every visit)
